5a05d13192bdf36d49d25c295c13e07d0dd2b3cb
Jacob Frick <jacobfrick@jacobs-macbook-air-3.local>
2018-12-12 21:17:11 -0800

    Initial Commit

---
+++ CSC481-ORS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSC481-ORS
+//
+//  Created by Jacob Frick on 12/12/18.
+//  Copyright © 2018 Jacob Frick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CSC481-ORSTests/CSC481_ORSTests.swift
@@ -0,0 +1,34 @@
+//
+//  CSC481_ORSTests.swift
+//  CSC481-ORSTests
+//
+//  Created by Jacob Frick on 12/12/18.
+//  Copyright © 2018 Jacob Frick. All rights reserved.
+//
+
+import XCTest
+@testable import CSC481_ORS
+
+class CSC481_ORSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
